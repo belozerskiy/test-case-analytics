de8f198f6f79338542e5c24235e3e4b86fa29c08
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-01-31 17:20:09 +0300

    Add UIkit framework

--- view/src/main.js
+++ view/src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+// UIKit framework CSS and JS files
+import "../node_modules/uikit/dist/css/uikt.css";
+import "../node_modules/uikit/dist/js/uikit.js";
+import "../node_modules/uikit/dist/js/uikit-icons.js";
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
